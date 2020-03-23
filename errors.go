de7929c8025de6ca67e953f94f4b225e532fc4b7
fobilow <fobilow@gmail.com>
2020-03-23 23:25:08 +0000

    fix typo

--- errors.go
+++ errors.go
@@ -6,6 +6,6 @@ var (
 	dbError                error = errors.New("Database error")
 	badBlockError          error = errors.New("Bad Block error - invalid json")
 	badRecordError         error = errors.New("Bad Record error")
-	connectionClosedError  error = errors.New("Connection is clossd")
+	connectionClosedError  error = errors.New("Connection is closed")
 	connectionInvalidError error = errors.New("Connection is not valid. use gitdb.Start to construct a valid connection")
 )

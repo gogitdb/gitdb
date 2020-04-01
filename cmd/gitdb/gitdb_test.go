02cd662b16a682ad2b7d1672c7ff3871475e5888
fobilow <fobilow@gmail.com>
2020-04-01 12:08:18 +0100

    Remove generated file after test

--- cmd/gitdb/gitdb_test.go
+++ cmd/gitdb/gitdb_test.go
@@ -1,11 +1,14 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func Test_embedUI(t *testing.T) {
 	err := embedUI()
 	if err != nil {
 		t.Errorf("embedUI() failed: %s", err)
 	}
-
+	os.Remove("./ui_gitdb.go")
 }

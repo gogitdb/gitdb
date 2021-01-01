ce072290b4959955b8114ab3843a287a7036d50b
Oke Ugwu <oke@fortifi.io>
2021-01-01 12:31:24 +0000

    remove logs

--- index.go
+++ index.go
@@ -132,16 +132,13 @@ func (g *gitdb) buildIndexSmart(changedFiles []string) {
 func (g *gitdb) buildIndexTargeted(target string) {
 	ds := db.LoadDataset(filepath.Join(g.dbDir(), target), g.config.EncryptionKey)
 	for _, block := range ds.Blocks() {
-		log.Info("build index for block: " + block.Path() + ", " + block.Dataset().Name() + "," + block.Dataset().Path())
 		g.updateIndexes(block)
 	}
 }
 
 func (g *gitdb) buildIndexFull() {
-	log.Info("db dir = " + g.dbDir())
 	datasets := db.LoadDatasets(g.dbDir(), g.config.EncryptionKey)
 	for _, ds := range datasets {
-		log.Info("build index for dataset: " + ds.Name())
 		g.buildIndexTargeted(ds.Name())
 	}
 	g.flushIndex()
